3fb7b0ad1a99690ddbd4c8efe88db414b0575165
Ndobito <jcuellarmenza@gmail.com>
2023-10-25 10:05:42 -0500

    Interface

--- view/calculator.php
+++ view/calculator.php
@@ -13,24 +13,38 @@
 <body>
 
     <div class="calculator">
-        <input type="text" id="display" readonly>
-        <button class="btn" onclick="clearDisplay()">C</button>
-        <button class="btn" onclick="numberClick('7')">7</button>
-        <button class="btn" onclick="numberClick('8')">8</button>
-        <button class="btn" onclick="numberClick('9')">9</button>
-        <button class="btn" onclick="numberClick('+')">+</button>
-        <button class="btn" onclick="numberClick('4')">4</button>
-        <button class="btn" onclick="numberClick('5')">5</button>
-        <button class="btn" onclick="numberClick('6')">6</button>
-        <button class="btn" onclick="numberClick('-')">-</button>
-        <button class="btn" onclick="numberClick('1')">1</button>
-        <button class="btn" onclick="numberClick('2')">2</button>
-        <button class="btn" onclick="numberClick('3')">3</button>
-        <button class="btn" onclick="numberClick('*')">*</button>
-        <button class="btn" onclick="numberClick('0')">0</button>
-        <button class="btn" onclick="numberClick('.')">.</button>
-        <button class="btn" onclick="calculateResult()">=</button>
-        <button class="btn" onclick="numberClick('/')">/</button>
+        <div>
+            <input type="text" id="display" readonly>
+        </div>
+        <div>
+            <button class="btn" onclick="clearDisplay()">C</button>
+        </div>
+        <div>
+
+            <button class="btn" onclick="numberClick('7')">7</button>
+            <button class="btn" onclick="numberClick('8')">8</button>
+            <button class="btn" onclick="numberClick('9')">9</button>
+            <button class="btn opr" onclick="numberClick('+')">+</button>
+        </div>
+        <div>
+            <button class="btn" onclick="numberClick('4')">4</button>
+            <button class="btn" onclick="numberClick('5')">5</button>
+            <button class="btn" onclick="numberClick('6')">6</button>
+            <button class="btn opr" onclick="numberClick('-')">-</button>
+        </div>
+        <div>
+            <button class="btn" onclick="numberClick('1')">1</button>
+            <button class="btn" onclick="numberClick('2')">2</button>
+            <button class="btn" onclick="numberClick('3')">3</button>
+            <button class="btn opr" onclick="numberClick('*')">*</button>
+        </div>
+        <div>
+            <button class="btn" onclick="numberClick('0')">0</button>
+            <button class="btn opr" onclick="numberClick('.')">.</button>
+            <button class="btn opr" onclick="calculateResult()">=</button>
+            <button class="btn opr" onclick="numberClick('/')">/</button>
+        </div>
+
     </div>
 
     <script src="assets/js/calculator.js"></script>
