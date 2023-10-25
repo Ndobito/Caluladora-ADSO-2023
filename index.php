f3c31e73e9beb971fa1342edd2b1cc9ac5408cc4
Ndobito <jcuellarmenza@gmail.com>
2023-10-25 08:27:00 -0500

    logica Base Completa - Arreglos en index.php

--- index.php
+++ index.php
@@ -3,12 +3,18 @@
 $controller = "index";
 
 if(!isset($_REQUEST['x'])){
-    require_once 'controller/'.$controller.'.controller.php'; 
+    require_once "controller/$controller.controller.php"; 
     $controller = 'Controller_'.ucwords($controller);
     $controller = new $controller;
     $controller->Inicio(); 
 }else{
-    
+    $controller = strtolower($_REQUEST['x']);
+    $action = isset($_REQUEST['y']) ? $_REQUEST['y'] :'Inicio';
+    $param = isset($_REQUEST['z']) ? $_REQUEST['z'] :'';
+    require_once "controller/$controller.controller.php";
+    $controller = 'Controller_'.ucwords($controller);
+    $controller = new $controller();
+    call_user_func_array(array($controller, $action), $param);
 }
 
 ?>
